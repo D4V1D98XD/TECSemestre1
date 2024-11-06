029c2018ab9b3ee1d5157ee56c68b3aa95139e51
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2024-11-06 16:54:49 -0700

    Add files via upload

---
+++ Unidad 3/problema19.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner; //6677122489
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String numeros, formato = "";
+		
+		for(int i=0; i<8; i++) {
+			numeros = scan.next();
+			if(numeros.length() == 10) {
+				formato = "(" + numeros.substring(0,3) + ")" + numeros.substring(3,6) + "-" + numeros.substring(6,8) + "-" + numeros.substring(8,10);
+			}
+			System.out.println(formato);
+		}
+	}
+
+}
