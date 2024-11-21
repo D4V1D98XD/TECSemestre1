a6ff555bf0ef4358b8513a8be66e2aecdc327c25
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2024-11-21 16:57:54 -0700

    Update Metodos2.java

--- unidadArreglos/src/Parte2/Metodos2.java
+++ unidadArreglos/src/Parte2/Metodos2.java
@@ -37,11 +37,18 @@ public class Metodos2 {
 	
 	public static int[] valoresDiferentes(int[] arr1, int[] arr2) {
 		int tamano = 0;
+		boolean encontrado = false;
 		for(int i=0; i<arr1.length; i++) {
+			encontrado = false;
 			for(int j=0; j<arr1.length; j++) {
 				if(arr1[i] != arr2[j]) {
-					tamano++;
+					System.out.println("wep");
+					encontrado = true;
+					break;
 				}
+			}
+			if(encontrado) {
+				tamano++;
 			}
 		}
 		System.out.println("tamano "+ tamano);
@@ -51,7 +58,8 @@ public class Metodos2 {
 		for(int i=0; i<arr1.length; i++) {
 			for(int j=0; j<arr1.length; j++) {
 				if(arr1[i] != arr2[j]) {
-					diferente[tamano] = arr2[j];
+					diferente[tamano] = arr1[i];
+					tamano++;
 				}
 			}
 			
